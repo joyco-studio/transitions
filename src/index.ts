abd634cb26bdb337ed07666401e21617da19ff85
Matias Perez <matiasperezpc@hotmail.com>
2025-01-24 12:10:25 -0300

    Remove unused constant 'foo' from src/index.ts and clean up Welcome component by eliminating its reference. This enhances code clarity and reduces unnecessary imports.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { version } from '../package.json'
 
 export const VERSION = version
-export const foo = 'bar'
 export * from './core'
--- templates/react-router/app/welcome/welcome.tsx
+++ templates/react-router/app/welcome/welcome.tsx
@@ -1,10 +1,7 @@
-import { foo } from '@joycostudio/transitions'
-
 import logoDark from './logo-dark.svg'
 import logoLight from './logo-light.svg'
 
 export function Welcome() {
-  console.log('foo', foo)
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
